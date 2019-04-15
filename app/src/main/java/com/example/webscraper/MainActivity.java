17479bde112d3f0a0a2286a06f5686ca153a5c5c
jesuscamacho <39933621+jesuscamacho@users.noreply.github.com>
2019-04-15 17:40:09 -0400

    Initial commit

---
+++ app/src/main/java/com/example/webscraper/MainActivity.java
@@ -0,0 +1,70 @@
+package com.example.webscraper;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button getBtn;
+    private TextView result;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setContentView(R.layout.activity_main);
+        result = (TextView) findViewById(R.id.result);
+        getBtn = (Button) findViewById(R.id.getBtn);
+        getBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getWebsite();
+            }
+        });
+    }
+
+    private void getWebsite() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                final StringBuilder builder = new StringBuilder();
+
+                try {
+                    Document doc = Jsoup.connect("https://dining.unc.edu/locations/chase/?date=2019-04-15").get();
+                    String title = doc.title();
+                    Elements links = doc.select("a[href]");
+
+                    builder.append(title).append("\n");
+
+                    for (Element link : links) {
+                        builder.append("\n").append("Link : ").append(link.attr("href"))
+                                .append("\n").append("Text : ").append(link.text());
+                    }
+                } catch (IOException e) {
+                    builder.append("Error : ").append(e.getMessage()).append("\n");
+                }
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        result.setText(builder.toString());
+                    }
+                });
+            }
+        }).start();
+    }
+
+
+
+
+}
