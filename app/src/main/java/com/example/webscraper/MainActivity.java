40e8c9fc2e72547bc093381465c141ff312c4e8a
jesuscamacho <39933621+jesuscamacho@users.noreply.github.com>
2019-04-16 21:05:14 -0400

    nav bar working!

--- app/src/main/java/com/example/webscraper/MainActivity.java
+++ app/src/main/java/com/example/webscraper/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.webscraper;
 
 import android.content.ClipData;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -17,11 +23,27 @@ public class MainActivity extends AppCompatActivity {
 
     private Button getBtn;
     private TextView result;
-    //private Item home;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.nav_search:
+                    Intent t = new Intent(MainActivity.this, Search.class);
+                    startActivity(t);
+                    finish();
+                    return true;
+            }
+            return false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        overridePendingTransition(0, 0);
         setContentView(R.layout.activity_main);
 
         //setContentView(R.layout.activity_main);
@@ -34,8 +56,8 @@ public class MainActivity extends AppCompatActivity {
                 getWebsite();
             }
         });
-
-        //home = findViewById(R.id.navigation_home);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
 
     }
--- app/src/main/java/com/example/webscraper/Search.java
+++ app/src/main/java/com/example/webscraper/Search.java
@@ -1,13 +1,51 @@
 package com.example.webscraper;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.View;
 
 public class Search extends AppCompatActivity {
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.nav_home:
+                    Intent t = new Intent(Search.this, MainActivity.class);
+                    startActivity(t);
+                    finish();
+                    return true;
+            }
+            return false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+        overridePendingTransition(0, 0);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        navigation.getMenu().getItem(1).setChecked(true);
+
+
+    }
+
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.nav_search){
+            Intent t = new Intent(Search.this, MainActivity.class);
+            startActivity(t);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
